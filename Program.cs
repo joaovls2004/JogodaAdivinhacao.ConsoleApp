38ef3cf2f1bb2e529ae735ee38493f527e39ba85
joão vinicius <joaovls204@gmail.com>
2026-03-21 17:37:33 -0300

    adicionado projeto inicial

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("-------------------------------------");
+Console.WriteLine("jogo de Adivinhação");
+Console.WriteLine("-------------------------------------");
+
+Console.ReadLine();
